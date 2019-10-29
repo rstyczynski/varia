f0b3db7e15c47769bd3963931708345118163a7b
rstyczynski <rstyczynski@gmail.com>
2019-10-29 14:55:35 +0100

    Create test_arabic.sh

---
+++ test_arabic.sh
@@ -0,0 +1,7 @@
+cd /tmp
+rm arabic.java
+wget https://raw.githubusercontent.com/rstyczynski/varia/master/arabic.java
+java_bin=$(ps aux | grep java | grep bin/java | tr -s ' ' | cut -d' ' -f11 | grep -v grep | sort -u)
+$java_bin\c -encoding UTF-8 arabic.java 
+$java_bin arabic
+$java_bin -Dfile.encoding="UTF-8" arabic
