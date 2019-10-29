d1bcfa5c71b22864b890f730dcedb8487925e559
rstyczynski <rstyczynski@gmail.com>
2019-10-29 15:10:47 +0100

    Update test_arabic.sh

--- test_arabic.sh
+++ test_arabic.sh
@@ -5,5 +5,16 @@ java_bin=$(ps aux | grep java | grep bin/java | tr -s ' ' | cut -d' ' -f11 | gre
 $java_bin\c -encoding UTF-8 arabic.java 
 $java_bin arabic
 $java_bin -Dfile.encoding="UTF-8" arabic
+
+export LANG=en_US.UTF-8; $java_bin\c arabic.java; unset LANG; $java_bin arabic 
+export LANG=en_US.UTF-8; $java_bin\c arabic.java; export LANG=en_US; $java_bin arabic 
+export LANG=en_US.UTF-8; $java_bin\c arabic.java; export LANG=en_US; $java_bin -Dfile.encoding="UTF-8" arabic 
+export LANG=en_US.UTF-8; $java_bin\c arabic.java; export LANG=en_US.UTF-8; $java_bin arabic
+export LANG=en_US.UTF-8; $java_bin\c arabic.java; export LANG=en_US.UTF-16; $java_bin arabic
+export LANG=en_US.UTF-8; $java_bin\c arabic.java; export LANG=en_US.UTF-16; $java_bin -Dfile.encoding="UTF-8" arabic 
+export LANG=en_US.UTF-8; $java_bin\c arabic.java; export LANG=ar_SA.arabic8; $java_bin arabic
+export LANG=en_US.UTF-8; $java_bin\c arabic.java; export LANG=ar_SA.arabic8; $java_bin -Dfile.encoding="UTF-8" arabic
+unset LANG
+
 rm arabic.class
 rm arabic.java
